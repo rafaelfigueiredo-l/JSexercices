ac24caedceb062eb8672e6cb6ab929f06947dbc9
rafaelfigueiredo-l <rafaael.figueiredo@gmail.com>
2019-12-10 18:02:01 -0300

    1 file modified

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function maxChar(str){
 
 function fizzBuzz(int){
   for(let i = 0; i < int; i++){
-    if(i % 3 === 0 && i % 5 === 0){
+    if(i % 3 === 0 && i % 5 === 0 &&){
       console.log('fizzbuzz');
     } else {
       if(i % 3 === 0) console.log('fizz');
@@ -135,4 +135,4 @@ maxChar('rafafafafafaffafafafafafafaafafaff');
 
 console.log('%c FizzBuzz | Write a program that console logs the numbers from 1 to n. But for multiples of three print "fizz" instead of the number and for the multiples of five print "buzz". For numbers which are multiples of both three and five print "fizzbuzz".', 'color: green; font-weight: bold');
 
-fizzBuzz(10);
+fizzBuzz(20);
